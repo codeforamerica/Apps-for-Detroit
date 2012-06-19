5d7713c6e43191fccfb55c88aa58a7a8c677de39
Prashant Singh <prashant@codeforamerica.org>
2012-06-18 19:38:59 -0700

    Remove accelerator form stuff

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
 $(document).ready(function() {
 
-	var show_application = function(){
-		$("#information").hide();
-		$("#application").show();
-		window.scrollTo(0,500);
-	}
 
-	$("#application").hide(); 
-
-	$(".Apply .button").live( 'click', function() {
-		show_application();
-		window.location.hash = '#application';
-		
-		
-		//Firefox for some reason does not refresh the DOM in iFrame
-	    if($.browser.mozilla){
-			document.getElementsByTagName('iframe')[0].src=document.getElementsByTagName('iframe')[0].src
-	    }
-
-	});
-
-
-
-
-
-	$("div#application a.back").click(function() {
-		$("#application").hide();
-		$("#information").show();
-		window.location.hash = '';
-	});
-		
 	$('.advisorimages ul li img').popover({
 		placement: 'bottom',
 		animation: false}
@@ -49,13 +20,12 @@ $(document).ready(function() {
 		easing : "easeInOutExpo",
 		hash : true
 	});
-		
-		
-	if(window.location.hash == '#application'){
-		show_application();
-	}
-	
-		
+
+  // Button clicks
+  $('#learn-more').click(function (e) {
+    e.preventDefault();
+    window.location = 'http://appsfordetroit.eventbrite.com/';
+  });
 
 
 });;
